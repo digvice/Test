fe96e406ae52ad5b58138d8ad859cf45249e45ff
digvice <digvice81@gmail.com>
2017-12-08 17:01:19 +0200

    Investor finished

---
+++ js/clipboard.js
@@ -0,0 +1,9 @@
+// Copy text to clipboard
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  // add this to element you want to copy to clipboard: onclick="copyToClipboard('#elementId')
